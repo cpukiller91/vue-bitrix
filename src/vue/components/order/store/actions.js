5f42f9b451a95628e5c79f16f4260ee888b5d195
Хомич Юрий <cpukiller@MacBook-Pro-Homic.local>
2021-12-05 18:20:10 +0200

    Delivery test

--- src/vue/components/order/store/actions.js
+++ src/vue/components/order/store/actions.js
@@ -166,7 +166,7 @@ export default {
         })
         .then(() => {
           commit('SET_AJAX_PROCESS', false)
-          //console.log("SET_AJAX_PROCESS",response)
+          console.log("SET_AJAX_PROCESS",response)
         })
     })
   }
